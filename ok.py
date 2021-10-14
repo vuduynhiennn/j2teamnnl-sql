b916dd61f9e9cc73a53dad43e45ced0dfa9ceeea
Vũ Duy Nhiên <88360269+vuduynhiennn@users.noreply.github.com>
2021-10-14 20:01:19 +0700

    file python

--- ok.py
+++ ok.py
@@ -1,1 +1,121 @@
 print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))print("hello I'm learning sql")
+#!/usr/bin/env python
+import math
+c=50
+h=30
+value = []
+items=[x for x in input("Nhập giá trị của d: ").split(',')]
+for d in items:
+    value.append(str(int(round(math.sqrt(2*c*float(d)/h)))))
+# Code by Quantrimang.com
+print (','.join(value))
